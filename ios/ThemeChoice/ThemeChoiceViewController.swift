6988bf4a580789ad4273cd06d27db5679fe4d48f
litong <ben2740@naver.com>
2020-09-07 16:43:07 +0900

    ThemeChoiceView Layout 80%
    디자이너가 Asset을 추가안해줘서 더 진행 못함 쪼아야함

---
+++ ios/ThemeChoice/ThemeChoiceViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ThemeChoiceViewController.swift
+//  ios
+//
+//  Created by 이동연 on 07/09/2020.
+//  Copyright © 2020 Jerry Jung. All rights reserved.
+//
+
+import UIKit
+
+class ThemeChoiceViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

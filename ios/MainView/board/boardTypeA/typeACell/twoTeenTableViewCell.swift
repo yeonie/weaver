f21d32e19f3ec9d522710e12adac2b7494a4d987
litong <ben2740@naver.com>
2020-10-27 09:32:09 +0900

    테이블 뷰 첨가

---
+++ ios/MainView/board/boardTypeA/typeACell/twoTeenTableViewCell.swift
@@ -0,0 +1,37 @@
+//
+//  twoTeenTableViewCell.swift
+//  ios
+//
+//  Created by 이동연 on 26/10/2020.
+//  Copyright © 2020 Jerry Jung. All rights reserved.
+//
+
+import UIKit
+
+class twoTeenTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var contentTitle: UILabel!
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var nickname: UILabel!
+    @IBOutlet weak var date: UILabel!
+    @IBOutlet weak var contentImage: UIImageView!
+    @IBOutlet weak var totalContent: UILabel!
+    @IBOutlet weak var likeNumb: UILabel!
+    @IBOutlet weak var replyNumb: UILabel!
+    
+    
+    @IBAction func replyBtnPressed(_ sender: UIButton) {
+        
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+}
---
+++ ios/MainView/board/boardTypeB/typeBcell/ENFPTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ENFPTableViewCell.swift
+//  ios
+//
+//  Created by 이동연 on 26/10/2020.
+//  Copyright © 2020 Jerry Jung. All rights reserved.
+//
+
+import UIKit
+
+class ENFPTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}

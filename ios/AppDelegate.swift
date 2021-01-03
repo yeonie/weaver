d0f065ef92c08aee548fff0880aa9c6dfa2b9ba5
litong <ben2740@naver.com>
2021-01-03 16:21:42 +0900

    testUpload

--- ios/AppDelegate.swift
+++ ios/AppDelegate.swift
@@ -37,7 +37,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         //FirebaseApp.configure()
         
         self.window = UIWindow(frame: UIScreen.main.bounds)
-        let splashViewController = SplashChoiceViewController()
+        let splashViewController = onboardingViewController()
         self.window?.rootViewController = UINavigationController(rootViewController: splashViewController)
         self.window?.makeKeyAndVisible()
 

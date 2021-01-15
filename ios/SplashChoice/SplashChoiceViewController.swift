2516d422b55337159357513b867312d1c7bdd05d
litong <litong4379@gmail.com>
2021-01-15 13:04:46 +0900

    진짜 키보드 완벽수정 && 이미지 필요한 에셋 추가

--- ios/SplashChoice/SplashChoiceViewController.swift
+++ ios/SplashChoice/SplashChoiceViewController.swift
@@ -10,20 +10,14 @@ import UIKit
 
 class SplashChoiceViewController: BaseViewController, UITextFieldDelegate {
     
-//    앱 딜리게이트-> 키 값은 아무거나 해서
-    
-    
-    
-        
-    var slided = true
+    var slidedS = true
+    var slidedL = true
     var slideoff = false
     var slideoffForSignIn = true
-    
     var ShowHide = true
-    
 
-    @IBOutlet weak var slideSignInView: UIView!
-    @IBOutlet weak var slideSginUpView: UIView!
+    @IBOutlet weak var slideLoginView: UIView!
+    @IBOutlet weak var slideSignUpView: UIView!
     @IBOutlet weak var giveMeRightRex: UILabel!
     @IBOutlet weak var giveMeRightRexSU: UILabel!
     //    로그인 텍스트 박스
@@ -43,11 +37,9 @@ class SplashChoiceViewController: BaseViewController, UITextFieldDelegate {
     @IBAction func PwShowHide(_ sender: UIButton) {
         if ShowHide{
             passwordBoxSU.isSecureTextEntry = false
-
         }
         else{
             passwordBoxSU.isSecureTextEntry = true
-
         }
         ShowHide = !ShowHide
     }
@@ -58,13 +50,12 @@ class SplashChoiceViewController: BaseViewController, UITextFieldDelegate {
         MainDataManager().getLogin(self)
     }
    
-    
     @IBAction func signUpBtnPressed(_ sender: Any) {
         let ownTypeChoiceViewController = OwnTypeChoiceViewController()
         ownTypeChoiceViewController.splashChoiceViewController = self
         navigationController?.pushViewController(ownTypeChoiceViewController, animated: true)
-
     }
+    
         //프로필 사진 박기
     @IBAction func profileBtnPressed(_ sender: UIButton) {
         let alert =  UIAlertController(title: "원하는 타이틀", message: "원하는 메세지", preferredStyle: .actionSheet)
@@ -81,10 +72,9 @@ class SplashChoiceViewController: BaseViewController, UITextFieldDelegate {
         alert.addAction(camera)
         alert.addAction(cancel)
         present(alert, animated: true, completion: nil)
-
     }
     
-    //    로그인형식
+    //    로그인 Regex
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
 //        로그인part
         if isValidEmail(emailBox: emailBox.text!) && passwordBox.text!.count > 3 {
@@ -106,25 +96,20 @@ class SplashChoiceViewController: BaseViewController, UITextFieldDelegate {
             signUpBtn.isEnabled = true
             giveMeRightRexSU.isHidden = false
         }
-        
         print(emailBox.text!, passwordBox.text!, range.location, range.length)
-        print(username.text!,nicknameBoxSU.text!, passwordBoxSU.text!, range.location, range.length) // 이 부분은 확인을 위해 만든 부분입니다.
+        print(username.text!,nicknameBoxSU.text!, passwordBoxSU.text!, range.location, range.length)
         return true
     }
     
 
     
     func isValidEmail(emailBox: String) -> Bool {
-        
         let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}"
-        
         let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
         return emailTest.evaluate(with: emailBox)
     }
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-        if let moviesVC = segue.destination as? BoardHomeViewController {
+            if let moviesVC = segue.destination as? BoardHomeViewController {
             if let text = emailBox.text {
                 moviesVC.queryText = text
             }
@@ -132,53 +117,32 @@ class SplashChoiceViewController: BaseViewController, UITextFieldDelegate {
     }
     
     
-    @IBAction func slideOffSginIn(_ sender: UIButton) {
-        
-        if slideoffForSignIn{
-            UIView.animate(withDuration: 0.4, animations:({
-                self.slideSginUpView.transform = CGAffineTransform(translationX: 0, y: -450)
-                self.slideSginUpView.layer.shadowColor = UIColor.black.cgColor
-                self.slideSginUpView.layer.shadowOffset = CGSize(width: 1, height: 2)
-                self.slideSginUpView.layer.shadowOpacity = 1
-                self.slideSginUpView.layer.shadowRadius = 100
-            }))
-        }
-        else{
-            UIView.animate(withDuration: 0.4, animations:({
-                self.slideSginUpView.transform = CGAffineTransform(translationX: 0, y: 0)
-                self.slideSignInView.layer.shadowOffset = CGSize(width: 0, height: 0)
-                self.slideSginUpView.layer.shadowOffset = CGSize(width: 0, height: 0)
-                self.slideSginUpView.layer.shadowColor = UIColor.white.cgColor
-            }))
-        }
-        slideoffForSignIn = !slideoffForSignIn
-    }
     //슬라이드 내려뿌기
     @IBAction func slideOff(_ sender: UIButton) {
         UIView.animate(withDuration: 0.4, animations:({
-            self.slideSignInView.transform = CGAffineTransform(translationX: 0, y: 0)
-                            }))
-        UIView.animate(withDuration: 0.4, animations:({
-            self.slideSginUpView.transform = CGAffineTransform(translationX: 0, y: 0)
-            self.slideSignInView.layer.shadowColor = UIColor.black.cgColor
+            self.slideSignUpView.transform = CGAffineTransform(translationX: 0, y: 0)
+            self.slideSignUpView.layer.shadowColor = UIColor.black.cgColor
         }))
-        self.slideSignInView.layer.shadowColor = UIColor.white.cgColor
-        self.slideSginUpView.layer.shadowColor = UIColor.white.cgColor
-        self.slideSignInView.layer.shadowOffset = CGSize(width: 0, height: 0)
-        self.slideSginUpView.layer.shadowOffset = CGSize(width: 0, height: 0)
+        
+        UIView.animate(withDuration: 0.4, animations:({
+            self.slideLoginView.transform = CGAffineTransform(translationX: 0, y: 0)
+            self.slideLoginView.layer.shadowColor = UIColor.black.cgColor
+        }))
+        self.slideSignUpView.layer.shadowColor = UIColor.white.cgColor
+        self.slideLoginView.layer.shadowColor = UIColor.white.cgColor
+        self.slideSignUpView.layer.shadowOffset = CGSize(width: 0, height: 0)
+        self.slideLoginView.layer.shadowOffset = CGSize(width: 0, height: 0)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
+
         emailBox.delegate = self
         passwordBox.delegate = self
         username.delegate = self
         passwordBoxSU.delegate = self
         nicknameBoxSU.delegate = self
         
-//        let username: String = username
         let password: String = passwordBoxSU.text!
         let nickname: String = nicknameBoxSU.text!
 //        수정
@@ -194,43 +158,41 @@ class SplashChoiceViewController: BaseViewController, UITextFieldDelegate {
         
         
         //keyboard UI
-//        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillAppear(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
-//
-//        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillDisappear(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
-        
-        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillAppear(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillDisappear(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
 
     }
-    private func addKeyboardNotification() {
-        NotificationCenter.default.addObserver(
-          self,
-          selector: #selector(keyboardWillShow),
-          name: UIResponder.keyboardWillShowNotification,
-          object: nil
-        )
-        
-        NotificationCenter.default.addObserver(
-          self,
-          selector: #selector(keyboardWillHide),
-          name: UIResponder.keyboardWillHideNotification,
-          object: nil
-        )
-      }
+//    private func addKeyboardNotification() {
+//        NotificationCenter.default.addObserver(
+//          self,
+//          selector: #selector(keyboardWillShow),
+//          name: UIResponder.keyboardWillShowNotification,
+//          object: nil
+//        )
+//
+//        NotificationCenter.default.addObserver(
+//          self,
+//          selector: #selector(keyboardWillHide),
+//          name: UIResponder.keyboardWillHideNotification,
+//          object: nil
+//        )
+//      }
     
     //keyboard UI 가리기/보여주기
-    @objc private func keyboardWillShow(_ notification: Notification) {
+    @objc private func keyboardWillAppear(_ notification: Notification) {
       if let keyboardFrame: NSValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue {
         let keybaordRectangle = keyboardFrame.cgRectValue
         let keyboardHeight = keybaordRectangle.height
-        self.view.frame.origin.y -= keyboardHeight
+        self.view.frame.origin.y = -keyboardHeight
       }
     }
       
-    @objc private func keyboardWillHide(_ notification: Notification) {
+    @objc private func keyboardWillDisappear(_ notification: Notification) {
       if let keyboardFrame: NSValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue {
         let keybaordRectangle = keyboardFrame.cgRectValue
         let keyboardHeight = keybaordRectangle.height
-        self.view.frame.origin.y += keyboardHeight
+        self.view.frame.origin.y = 0
       }
     }
     
@@ -253,41 +215,55 @@ class SplashChoiceViewController: BaseViewController, UITextFieldDelegate {
 
     }
 
-
-    
-    //가입하기
-    @IBAction func SignUpBtn(_ sender: UIButton) {
-        slided = !slided
-        
-        if slided{
+    //로그인
+    @IBAction func LoginBtn(_ sender: UIButton) {
+        slidedL = !slidedL
+        if slidedL{
             UIView.animate(withDuration: 0.3, animations:({
                 
-                self.slideSignInView.transform = CGAffineTransform(translationX: 0, y: 0)
-                self.slideSignInView.layer.shadowOffset = CGSize(width: 0, height: 0)
-                
-                
-                self.slideSginUpView.layer.shadowOffset = CGSize(width: 0, height: 0)
-                
-                self.slideSignInView.layer.shadowColor = UIColor.white.cgColor
-                
-                self.slideSginUpView.layer.shadowColor = UIColor.white.cgColor
-                
-            }))
-            //            self.backgroundColor.backgroundColor = UIColor.darkGray
-            
-            
+                self.slideLoginView.transform = CGAffineTransform(translationX: 0, y: 0)
+                self.slideLoginView.layer.shadowOffset = CGSize(width: 0, height: 0)
+                self.slideLoginView.layer.shadowOffset = CGSize(width: 0, height: 0)
+                self.slideLoginView.layer.shadowColor = UIColor.white.cgColor
+                self.slideLoginView.layer.shadowColor = UIColor.white.cgColor
+        }))
         }
         else{
             UIView.animate(withDuration: 0.3, animations:({
-                self.slideSignInView.transform = CGAffineTransform(translationX: 0, y: -520)
-                self.slideSignInView.layer.shadowColor = UIColor.black.cgColor
-                self.slideSignInView.layer.shadowOffset = CGSize(width: 1, height: 2)
-                self.slideSignInView.layer.shadowOpacity = 1
-                self.slideSignInView.layer.shadowRadius = 100
+                self.slideLoginView.transform = CGAffineTransform(translationX: 0, y: -450)
+                self.slideLoginView.layer.shadowColor = UIColor.black.cgColor
+                self.slideLoginView.layer.shadowOffset = CGSize(width: 1, height: 2)
+                self.slideLoginView.layer.shadowOpacity = 1
+                self.slideLoginView.layer.shadowRadius = 100
 //                self.mainFeedTableView.backgroundColor = UIColor.darkGray
             }))
-//            self.backgroundColor.backgroundColor = UIColor.clear
+        }
             
+        
+    }
+    
+    //가입하기
+    @IBAction func SignUpBtn(_ sender: UIButton) {
+        slidedS = !slidedS
+        
+        if slidedS{
+            UIView.animate(withDuration: 0.3, animations:({
+                
+                self.slideSignUpView.transform = CGAffineTransform(translationX: 0, y: 0)
+                self.slideSignUpView.layer.shadowOffset = CGSize(width: 0, height: 0)
+                self.slideSignUpView.layer.shadowOffset = CGSize(width: 0, height: 0)
+                self.slideSignUpView.layer.shadowColor = UIColor.white.cgColor
+                self.slideSignUpView.layer.shadowColor = UIColor.white.cgColor
+            }))
+        }
+        else{
+            UIView.animate(withDuration: 0.3, animations:({
+                self.slideSignUpView.transform = CGAffineTransform(translationX: 0, y: -520)
+                self.slideSignUpView.layer.shadowColor = UIColor.black.cgColor
+                self.slideSignUpView.layer.shadowOffset = CGSize(width: 1, height: 2)
+                self.slideSignUpView.layer.shadowOpacity = 1
+                self.slideSignUpView.layer.shadowRadius = 100
+            }))
         }
     }
     
